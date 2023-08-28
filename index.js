0d61b9e4aea8faca3019f4da61263de78cf99cb7
Jaime <quezadajaime@ymail.com>
2023-08-27 19:25:46 -0700

    index node working

--- index.js
+++ index.js
@@ -6,21 +6,35 @@ const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
-
     {
         type: 'input',
         name: 'github',
         message: 'Enter your GitHub Username (Required)',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username');
+                return false;
+            }
+        }
     },
     {
-        type: 'input'
-        name: 'emailAddress'
+        type: 'input',
+        name: 'emailAddress',
+        message: 'Please enter your email address.'
     }
 
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) throw new Error(err);
+
+        console.log('The README has been generated, please check the generated-readme file!')
+    })
+}
 
 // TODO: Create a function to initialize app
 function init() {
@@ -29,11 +43,11 @@ function init() {
     Please answer the following questions to help the app generate a README
     `);
 
-    inquirer.prompt(quetsions)
-    .then(readmeData) => {
-        writeToFile('./dist/readme.md', generateMarkdown(readmeData))
-    }
- }
+    inquirer.prompt(questions)
+        .then(readmeData => {
+            writeToFile('./generated-readme/readme.md', generateMarkdown(readmeData))
+        })
+};
 
 // Function call to initialize app
 init();
